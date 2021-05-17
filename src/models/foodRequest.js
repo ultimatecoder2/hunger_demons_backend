238d6ac1a5533f2e88f22aa096b8ad6fc5722a50
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-17 12:26:53 +0530

    food request schema added

---
+++ src/models/foodRequest.js
@@ -0,0 +1,65 @@
+const mongoose= require('mongoose');
+const validator = require('validator');
+
+const foodSchema = new mongoose.Schema({
+    requestType:{ // either one from needy/ donor
+        type:String,
+        required:true,
+        trim:true
+    },
+    foodType:[{
+        //One from freshly cooked or packed food or raw food
+        type: String,
+        trim:true,
+        required:true
+    }],
+    food_description:[{
+        foodName:{
+            type: String,
+            trim:true,
+            required:true
+        },
+        quantity:{
+            type: Number,
+            trim:true,
+            required:true
+        }
+        //It contains specific food like wheat, rice, etc...
+        
+    }],
+    address:[{
+        addressLine1:{
+            type:String,
+            trim: true
+        },
+        addressLine2:{
+            type:String,
+            trim: true
+        },
+        city:{
+            type:String,
+            trim: true
+        },
+        state:{
+            type:String,
+            trim: true
+        },
+        postalCode:{
+            type:String,
+            trim: true
+        },
+        country:{
+            type:String,
+            trim: true,
+            required:true
+        }
+    }],
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    }
+},{timestamps:true});
+
+const FoodRequest = mongoose.model('FoodRequest',foodSchema);
+module.exports = FoodRequest;

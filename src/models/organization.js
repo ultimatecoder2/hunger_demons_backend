d5eab88aae182302bcb8b7551a2bf59558dce0e3
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-19 11:10:17 +0530

    added organization schema

---
+++ src/models/organization.js
@@ -0,0 +1,67 @@
+const mongoose= require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Organization = require('./organization');
+const organizationSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true,
+        trim: true
+    },
+    foodType:[{
+        //One from freshly cooked or packed food or raw food
+        type: String,
+        trim:true,
+        required:true
+    }],
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    contact:{
+        type: String,
+        trim: true,
+    },
+    address:[{
+        addressLine1:{
+            type:String,
+            trim: true
+        },
+        addressLine2:{
+            type:String,
+            trim: true
+        },
+        city:{
+            type:String,
+            trim: true
+        },
+        state:{
+            type:String,
+            trim: true
+        },
+        postalCode:{
+            type:String,
+            trim: true
+        },
+        country:{
+            type:String,
+            trim: true
+        }
+    }]
+    
+},{timestamps:true});
+
+
+const Organizations = mongoose.model('Organization',organizationSchema);
+module.exports = Organizations;
+
+
